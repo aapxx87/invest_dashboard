40f8f8059e8b108aad3accb9184be415ab726df3
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2021-12-17 14:50:02 +0300

    base structure for invets mobile

---
+++ src/components/spinner/all_api_operations.js
@@ -0,0 +1,176 @@
+// создаем строку из тикеров массива tickerArr, которая будет вставляться в запрос АПИ по получению котировок
+import {renderSpinner} from "./spinner";
+import {investmentPortfolio} from "../../data_invest";
+import {componentStockData} from "../stock_card/stock_card";
+import {componentTotalBalances} from "../stock_summary/stock_summary";
+
+const create_StocksStringForAPI = function (arr) {
+
+    // апдейтнули тикеры в формат для запроса
+    const arrForFetchString = arr.map(function (el) {
+        return el + '%2C'
+    })
+
+    //склеили аргументы в строку для запроса котировок
+    const forFetchStocksString = arrForFetchString.join('')
+
+    return forFetchStocksString
+
+}
+
+
+
+
+
+// получаем курсы котировок единым запросом (акции + курс рубля)
+const get_API_StocksQuotes = async function (formattedQuotesString) {
+
+    console.log('Start fetch API stock data')
+
+
+    const response = await fetch(`https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=${formattedQuotesString}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "yh-finance.p.rapidapi.com",
+            "x-rapidapi-key": "8caa20872cmsh50a9c8ee420964ep1ae65ejsne134ec66d0a1"
+        }
+    })
+
+    const data = await response.json()
+
+    // получили массив с данными по акциям из массива аргументов
+    const stockQuotesArr = data.quoteResponse.result;
+
+    return stockQuotesArr
+
+}
+
+
+
+
+// вытягиваем текущее значение бенчмарка
+const get_BenchmarkLastPrice = function(arr) {
+
+    const benchMarkData = arr.filter(function(el) {
+        return el.symbol === 'FXIT.ME'
+    })
+
+    const lastBMPrice = benchMarkData[0].regularMarketPrice
+
+    return lastBMPrice
+}
+
+
+
+
+
+
+
+
+const calculate_StocksData = async function (arr) {
+
+    renderSpinner(containerPortfolioQuotes)
+
+    // получаем актуальные курсы котировок (акции + рубль - последний элемент)
+    const stocksPlusRubQuotes = await get_API_StocksQuotes(tickerArr)
+
+    // оставляем только массив с курсами котирвок, отрезали последний элемент - курс рубля
+    const stockQuotesArr = stocksPlusRubQuotes.slice(0, -1)
+
+    // получаем только данные по рублю
+    const rubUsdRate = stocksPlusRubQuotes.pop().regularMarketPrice
+
+    // переменная для формироания объекта с данными по балансам
+    let totalDataObj
+
+
+    // текущая цена бенчмарка (вытягиваем из всего списка котировок полученных через АПИ по всем инструментам)
+    const lastBMPrice = get_BenchmarkLastPrice(stocksPlusRubQuotes)
+
+
+
+    // проходимся по массиву с тикерами из портфеля и матчим их с полученными котировками по АПИ, чтобы вытянуть нужные данные
+    arr.forEach(function (el) {
+
+
+        // вытягиваем последнюю актуальную цену акции из объекта полученного по АПИ проходимся по массиву с полученными через API котировками и сравниваем с текущим элементом в массиве с тикерами (передаем как аргумент в функцию). Когда находим соответствие сохраняем последнюю цену в переменную  lastStockPrice
+        let lastStockPrice
+
+        stockQuotesArr.forEach(function (elApi) {
+            if (elApi.symbol === el.toUpperCase()) {
+                lastStockPrice = elApi.regularMarketPrice
+            }
+        })
+
+
+        //  находим объект со стартовыми данными по акции, потом потребуются для калькуляции других параметров
+        const currentStock = investmentPortfolio.filter(function (elPortfolio) {
+            return elPortfolio.stockTicker === el
+        })
+
+
+        const pnl = lastStockPrice - currentStock[0].buyPrice
+
+
+        if (currentStock[0].currency === 'usd') {
+            portfolioSumArr.push(+(currentStock[0].volume * lastStockPrice) * rubUsdRate).toFixed(2)
+            portfolioSumStartArr.push(currentStock[0].buyPrice * currentStock[0].volume * currentStock[0].usdRubRateBuyDate)
+        }
+
+
+        if (currentStock[0].currency === 'rub') {
+            portfolioSumArr.push(+(currentStock[0].volume * lastStockPrice).toFixed(2))
+            portfolioSumStartArr.push(currentStock[0].buyPrice * currentStock[0].volume)
+        }
+
+        const totalInvestmentsSum = portfolioSumArr.reduce(function (acc, cur) {
+            return acc + cur
+        }, 0).toFixed(2)
+
+        const totalInvestmentsSumStart = portfolioSumStartArr.reduce(function (acc, cur) {
+            return acc + cur
+        }, 0).toFixed(2)
+
+
+        // считаем процентное изменение бенчмарка
+        const benchmarkLastPrice = (lastBMPrice - currentStock[0].benchMarkStartPrice)/(currentStock[0].benchMarkStartPrice/100)
+
+
+        // формируем объект с данными, которые передадим в компоненту для формирвования HTML по каждой акции
+        const stockDataObj = {
+            pnl: pnl,
+            currency: currentStock[0].currency,
+            ticker: currentStock[0].stockTicker.toUpperCase(),
+            volume: currentStock[0].volume,
+            buyPrice: new Intl.NumberFormat('ru-RU').format(currentStock[0].buyPrice),
+            lastPrice: new Intl.NumberFormat('ru-RU').format(lastStockPrice.toFixed(2)),
+            totalVolumePrice: new Intl.NumberFormat('ru-RU').format((currentStock[0].volume * lastStockPrice).toFixed(2)),
+            deltaMoney: new Intl.NumberFormat('ru-RU').format((pnl * currentStock[0].volume).toFixed(2)),
+            deltaPercent: ((lastStockPrice - currentStock[0].buyPrice) / (currentStock[0].buyPrice / 100)).toFixed(2),
+            benchMarkDeltaPercent: benchmarkLastPrice.toFixed(0),
+            benchMarkName: "FXIT.ME",
+        }
+
+        // формируем объект с данными, которые передадим в компоненту для формирвования итоговых балансов
+        totalDataObj = {
+            startAmount: new Intl.NumberFormat('ru-RU').format(totalInvestmentsSumStart),
+            lastAmount: new Intl.NumberFormat('ru-RU').format(totalInvestmentsSum),
+            totalDelta: new Intl.NumberFormat('ru-RU').format(totalInvestmentsSum - totalInvestmentsSumStart),
+            totalDeltaPercent: ((totalInvestmentsSum - totalInvestmentsSumStart) / (totalInvestmentsSumStart / 100)).toFixed(0)
+        }
+
+        const elem = document.querySelector('.spinner');
+
+        elem.style.display = 'none'
+
+
+        // вставляем компоненту по каждой конкретной акции
+        componentStockData(stockDataObj)
+
+    })
+
+    // всавляем компоненту с балансами тотальными
+    componentTotalBalances(totalDataObj)
+
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+        port: '3000',
+        hot: true,
+        watchFiles: './',
+    },
+    target: "web",
+}
