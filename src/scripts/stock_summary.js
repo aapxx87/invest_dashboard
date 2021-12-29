8782eb77a5f7f380a69316ffac6e7b345478be86
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2021-12-29 10:53:20 +0300

    table header done

--- src/scripts/stock_summary.js
+++ src/scripts/stock_summary.js
@@ -2,9 +2,10 @@ const ulTotalBalances = document.querySelector('.portfolio-balances')
 
 export const componentTotalBalances = function (obj) {
 
+    // тут добавлен css класс last balance, так как именно по этому классы забирается значение из html для рассчета доходности портфеля текущего года
+
     const html = `
-       
-           <li class="item-row">
+           <li class="portfolio-balances-row">
             <p class="balances-item last-balance">${obj.lastAmount} P</p>
             <p class="balances-item">${obj.startAmount} P</p>
             <p class="balances-item">${obj.totalDeltaPercent} %</p>
